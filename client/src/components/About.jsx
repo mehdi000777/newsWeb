2be87af5bd6a7c1786f56f0da02859c41fb4ed66
mehdi000777 <mehdi000077777@gmail.com>
2024-12-11 19:00:50 +0330

    9 commit

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { SectionWrapper } from "../hoc";
-import AboutBG from "../assets/aboutBG.jpg";
+import AboutBG from "../assets/aboutBG.png";
 
 const About = () => {
   return (
-    <div className="flex items-center justify-between">
-      <img src={AboutBG} />
-      <div>
+    <div className="flex items-center lg:justify-between justify-center">
+      <img src={AboutBG} className="hidden lg:block" />
+      <div className="text-center lg:text-start">
         <h1 className="text-[5rem] text-primary">آیناز کرد</h1>
         <p className="text-[3rem]">
           کلاس <span className="text-lime-500">یازدهم </span>تجربی
